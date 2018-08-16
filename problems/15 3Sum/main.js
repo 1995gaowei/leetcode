0413d6792b8b8b142ea3b86465aea521c924aaf9
1995gaowei <15850535810@163.com>
2018-08-16 12:04:53 +0800

    test

--- problems/15 3Sum/main.js
+++ problems/15 3Sum/main.js
@@ -1,22 +1,24 @@
 var threeSum = function(nums) {
-    nums.sort((a, b) => a-b);
-    let res = [];
-    for (let i = 0; i < nums.length-2; i++) {
-        if ( i == 0 || nums[i] != nums[i-1]) {
-            let lo = i+1, hi = nums.length-1, target = 0 - nums[i];
-            while (lo < hi) {
-                if (nums[lo] + nums[hi] == target) {
-                    res.push(Array.of(nums[i], nums[lo], nums[hi]));
-                    while (lo < hi && nums[lo] == nums[lo+1]) lo++;
-                    while (lo < hi && nums[hi] == nums[hi+1]) hi--;
-                    lo++;
-                    hi--;
-                } else if (nums[lo] + nums[hi] < target) lo++;
-                else hi--;
-            }
-        }
+  nums.sort((a, b) => a - b);
+  let res = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i == 0 || nums[i] != nums[i - 1]) {
+      let lo = i + 1,
+        hi = nums.length - 1,
+        target = 0 - nums[i];
+      while (lo < hi) {
+        if (nums[lo] + nums[hi] == target) {
+          res.push(Array.of(nums[i], nums[lo], nums[hi]));
+          while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+          while (lo < hi && nums[hi] == nums[hi + 1]) hi--;
+          lo++;
+          hi--;
+        } else if (nums[lo] + nums[hi] < target) lo++;
+        else hi--;
+      }
     }
-    return res;
+  }
+  return res;
 };
 
-console.log(threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]));
+console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]));
